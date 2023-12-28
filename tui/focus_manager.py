3be51eef9e79688b970d333ed888e923b6a5e4bf
82746 <82746@proton.me>
2023-12-28 03:19:46 +0200

    Update FocusManager to use ComponentTree + add more functionality

--- tui/focus_manager.py
+++ tui/focus_manager.py
@@ -1,10 +1,12 @@
-from events import Event, EventType
-from components import Component
+from .events import Event, EventType
+from .components.component import Component
+from .components.component_tree import ComponentTree
 
 
 class FocusManager:
-    def __init__(self) -> None:
+    def __init__(self, component_tree: ComponentTree) -> None:
         self._focused_component: Component | None = None
+        self._component_tree = component_tree
 
     def remove_focus(self):
         if self._focused_component:
@@ -15,9 +17,42 @@ class FocusManager:
     def set_focus(self, component: Component):
         if self._focused_component:
             self._focused_component.enqueue_event(Event(EventType.FOCUS_OUT))
-        else:
-            self._focused_component = component
-            self._focused_component.enqueue_event(Event(EventType.FOCUS_IN))
+
+        self._focused_component = component
+        self._focused_component.enqueue_event(Event(EventType.FOCUS_IN))
+
+    def focus_first_focusable(self):
+        for c in self._component_tree.traverse_components():
+            if c.is_focusable is True:
+                self.set_focus(c)
+                break
+
+    def focus_next(self):
+        if self._focused_component is None:
+            self.focus_first_focusable()
+            return
+
+        is_after_current = False
+        for c in self._component_tree.traverse_components():
+            if c == self._focused_component:
+                is_after_current = True
+            elif c.is_focusable is True and is_after_current is True:
+                self.set_focus(c)
+                break
+
+    def focus_previous(self):
+        if self._focused_component is None:
+            self.focus_first_focusable()
+            return
+
+        comp_gen = self._component_tree.traverse_components()
+        prev_component = next(comp_gen)
+        for c in comp_gen:
+            if c == self._focused_component and prev_component.is_focusable is True:
+                self.set_focus(prev_component)
+                break
+            if c.is_focusable is True:
+                prev_component = c
 
     @property
     def focused_component(self):
