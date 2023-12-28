1394003ee2e2ed1dbc8752d2f69cc9cd267585f3
82746 <82746@proton.me>
2023-12-28 03:05:04 +0200

    Add alpha value to RGB -> RGBA

--- tui/color.py
+++ tui/color.py
@@ -1,16 +1,22 @@
-class RGB:
-    def __init__(self, r, g, b):
-        self._validate_value(r)
-        self._validate_value(g)
-        self._validate_value(b)
+class RGBA:
+    def __init__(self, r, g, b, a):
+        self._validate_rgb(r)
+        self._validate_rgb(g)
+        self._validate_rgb(b)
+        self._validate_alpha(a)
 
         self._r = r
         self._g = g
         self._b = b
+        self._a = a
 
-    def _validate_value(self, value):
-        if not (0 <= value <= 255):
+    def _validate_rgb(self, value):
+        if not 0 <= value <= 255:
             raise ValueError("RGB values must be in the range 0-255")
+
+    def _validate_alpha(self, value):
+        if not 0 <= value <= 1:
+            raise ValueError("Alpha value must be in the range 0-1")
 
     @property
     def r(self):
@@ -18,7 +24,7 @@ class RGB:
 
     @r.setter
     def r(self, new_value):
-        self._validate_value(new_value)
+        self._validate_rgb(new_value)
         self._r = new_value
 
     @property
@@ -27,7 +33,7 @@ class RGB:
 
     @g.setter
     def g(self, new_value):
-        self._validate_value(new_value)
+        self._validate_rgb(new_value)
         self._g = new_value
 
     @property
@@ -36,8 +42,17 @@ class RGB:
 
     @b.setter
     def b(self, new_value):
-        self._validate_value(new_value)
+        self._validate_rgb(new_value)
         self._b = new_value
+
+    @property
+    def a(self):
+        return self._b
+
+    @a.setter
+    def a(self, new_value):
+        self._validate_alpha(new_value)
+        self._a = new_value
 
 
 class HSL:
